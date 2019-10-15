aa2654bbfb25c35b697fe960385a65a9f42d43c5
Rickard Laurin <rickard.laurin@gmail.com>
2019-10-15 09:00:50 +0200

    fix: do not purge css from layout

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,13 @@
-const purgecss = require("@fullhuman/postcss-purgecss")({
-  content: ["./src/**/*.tsx", "./src/style.css"],
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  content: ['./src/**/*.tsx', './src/**/*.css'],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
 })
 
 module.exports = {
   plugins: [
-    require("tailwindcss"),
-    ...(process.env.NODE_ENV === "production"
-      ? [purgecss, require("cssnano")]
+    require('tailwindcss'),
+    ...(process.env.NODE_ENV === 'production'
+      ? [purgecss, require('cssnano')]
       : []),
   ],
 }
