dbd82bd3875dac82b0a7fb96cb5af5693f72164e
Rickard Natt och Dag <rickard@hey.com>
2021-01-22 16:13:50 +0100

    feat: add tag filtering

--- .eleventy.js
+++ .eleventy.js
@@ -34,6 +34,36 @@ module.exports = function (eleventyConfig) {
     return tags.filter((t) => t !== 'post')
   })
 
+  eleventyConfig.addCollection('tagList', (collection) => {
+    let tagSet = new Set()
+
+    collection.getAll().forEach(function (item) {
+      if ('tags' in item.data) {
+        let tags = item.data.tags
+
+        tags = tags.filter(function (item) {
+          switch (item) {
+            // this list should match the `filter` list in tags.njk
+            case 'all':
+            case 'nav':
+            case 'post':
+            case 'posts':
+              return false
+          }
+
+          return true
+        })
+
+        for (const tag of tags) {
+          tagSet.add(tag)
+        }
+      }
+    })
+
+    // returning an array in addCollection works in Eleventy 0.5.3
+    return [...tagSet]
+  })
+
   // Calculate days I've owned my keyboard
   eleventyConfig.addShortcode('keyboardDays', function () {
     const keyboardTime =
